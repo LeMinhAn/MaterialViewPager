1d0677ce0011b0f3d129b0f501c745aec45e17c7
LeMinhAn <leminhan.cst@gmail.com>
2016-06-10 17:35:09 +0700

    Commit first
    ABC

---
+++ sample/src/main/java/com/github/florent37/materialviewpager/sample/flowingdrawer_core/FlowingAnimationListener.java
@@ -0,0 +1,27 @@
+package com.github.florent37.materialviewpager.sample.flowingdrawer_core;
+
+
+import com.nineoldandroids.animation.Animator;
+
+public class FlowingAnimationListener implements Animator.AnimatorListener {
+
+    public void onAnimationStart(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animation) {
+
+    }
+
+}
